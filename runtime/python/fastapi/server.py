d873778fc1f894091cfea7cf44e51f709cc7bdda
马梓淇 <maxgameone@qq.com>
2025-05-24 17:44:42 +0800

    fix

--- runtime/python/fastapi/server.py
+++ runtime/python/fastapi/server.py
@@ -1,16 +1,3 @@
-# Copyright (c) 2024 Alibaba Inc (authors: Xiang Lyu)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import asyncio
 import os
 import queue
@@ -18,86 +5,97 @@ import sys
 import argparse
 import logging
 import threading
-logging.getLogger('matplotlib').setLevel(logging.WARNING)
 from fastapi import FastAPI, UploadFile, Form, File, WebSocket, WebSocketDisconnect
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append('{}/../../..'.format(ROOT_DIR))
 sys.path.append('{}/../../../third_party/Matcha-TTS'.format(ROOT_DIR))
 from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
 from cosyvoice.utils.file_utils import load_wav
-import uuid
-import logging
+
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
-
-# set cross region allowance
+model_dir = None
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"])
-user_sessions = {}
+
+def get_model(model_dir):
+    try:
+        return CosyVoice(model_dir)
+    except Exception:
+        return CosyVoice2(model_dir)
 
 def generate_data(model_output):
     for i in model_output:
         tts_audio = (i['tts_speech'].numpy() * (2 ** 15)).astype(np.int16).tobytes()
         yield tts_audio
 
-
 @app.get("/inference_sft")
 @app.post("/inference_sft")
 async def inference_sft(tts_text: str = Form(), spk_id: str = Form()):
-    model_output = cosyvoice.inference_sft(tts_text, spk_id)
+   
+    model = get_model(model_dir)
+    model_output = model.inference_sft(tts_text, spk_id)
     return StreamingResponse(generate_data(model_output))
-
 
 @app.get("/inference_zero_shot")
 @app.post("/inference_zero_shot")
 async def inference_zero_shot(tts_text: str = Form(), prompt_text: str = Form(), prompt_wav: UploadFile = File()):
+   
+    model = get_model(model_dir)
     prompt_speech_16k = load_wav(prompt_wav.file, 16000)
-    model_output = cosyvoice.inference_zero_shot(tts_text, prompt_text, prompt_speech_16k)
+    model_output = model.inference_zero_shot(tts_text, prompt_text, prompt_speech_16k)
     return StreamingResponse(generate_data(model_output))
-
 
 @app.get("/inference_cross_lingual")
 @app.post("/inference_cross_lingual")
 async def inference_cross_lingual(tts_text: str = Form(), prompt_wav: UploadFile = File()):
+    
+    model = get_model(model_dir)
     prompt_speech_16k = load_wav(prompt_wav.file, 16000)
-    model_output = cosyvoice.inference_cross_lingual(tts_text, prompt_speech_16k)
+    model_output = model.inference_cross_lingual(tts_text, prompt_speech_16k)
     return StreamingResponse(generate_data(model_output))
-
 
 @app.get("/inference_instruct")
 @app.post("/inference_instruct")
 async def inference_instruct(tts_text: str = Form(), spk_id: str = Form(), instruct_text: str = Form()):
-    model_output = cosyvoice.inference_instruct(tts_text, spk_id, instruct_text)
+   
+    model = get_model(model_dir)
+    model_output = model.inference_instruct(tts_text, spk_id, instruct_text)
     return StreamingResponse(generate_data(model_output))
-
 
 @app.get("/inference_instruct2")
 @app.post("/inference_instruct2")
 async def inference_instruct2(tts_text: str = Form(), instruct_text: str = Form(), prompt_wav: UploadFile = File()):
+  
+    model = get_model(model_dir)
     prompt_speech_16k = load_wav(prompt_wav.file, 16000)
-    model_output = cosyvoice.inference_instruct2(tts_text, instruct_text, prompt_speech_16k)
+    model_output = model.inference_instruct2(tts_text, instruct_text, prompt_speech_16k)
     return StreamingResponse(generate_data(model_output))
-
 
 @app.websocket("/ws_tts")
 async def websocket_tts(websocket: WebSocket):
     await websocket.accept()
     try:
+        
+        model = get_model(model_dir)
+        logging.info("模型实例已加载")
+
         prompt_wav_path = os.path.join(ROOT_DIR, "zero_shot_prompt.wav")
         with open(prompt_wav_path, "rb") as f:
             prompt_speech_16k = load_wav(f, 16000)
             logging.info("音色载入成功")
 
         text_queue = queue.Queue()
-        loop = asyncio.get_running_loop()  # 获取主线程事件循环
+        loop = asyncio.get_running_loop()
 
         async def receive_texts():
             while True:
@@ -122,7 +120,7 @@ async def websocket_tts(websocket: WebSocket):
         def tts_worker():
             logging.info("推理线程已启动")
             try:
-                for i, j in enumerate(cosyvoice.inference_zero_shot(
+                for i, j in enumerate(model.inference_zero_shot(
                         text_generator(),
                         "希望你以后能够做的比我还好呦。",
                         prompt_speech_16k,
@@ -131,7 +129,7 @@ async def websocket_tts(websocket: WebSocket):
                     logging.info("解析完毕")
                     fut = asyncio.run_coroutine_threadsafe(
                         websocket.send_bytes(tts_audio),
-                        loop  # 用主线程事件循环
+                        loop
                     )
                     fut.result()
                     logging.info("已发送音频数据: %s", i)
@@ -140,7 +138,6 @@ async def websocket_tts(websocket: WebSocket):
 
         threading.Thread(target=tts_worker, daemon=True).start()
 
-        # 保持连接直到客户端断开
         while True:
             await asyncio.sleep(0.1)
             if not websocket.client_state.name == "CONNECTED":
@@ -150,6 +147,7 @@ async def websocket_tts(websocket: WebSocket):
         await websocket.close()
     except Exception as e:
         await websocket.send_json({"error": str(e)})
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port',
@@ -160,11 +158,6 @@ if __name__ == '__main__':
                         default='iic/CosyVoice-300M',
                         help='local path or modelscope repo id')
     args = parser.parse_args()
-    try:
-        cosyvoice = CosyVoice(args.model_dir)
-    except Exception:
-        try:
-            cosyvoice = CosyVoice2(args.model_dir)
-        except Exception:
-            raise TypeError('no valid model_type!')
+     # 设置全局 model_dir
+    model_dir = args.model_dir
     uvicorn.run(app, host="0.0.0.0", port=args.port)
