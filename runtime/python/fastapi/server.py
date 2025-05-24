ca7a879968d6a880e11a75f6710e7364bb4b148d
马梓淇 <maxgameone@qq.com>
2025-05-24 22:47:57 +0800

    fix

--- runtime/python/fastapi/server.py
+++ runtime/python/fastapi/server.py
@@ -9,7 +9,8 @@ from fastapi import FastAPI, UploadFile, Form, File, WebSocket, WebSocketDisconn
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from tts_worker_process import run_inference
+from cosyvoice.utils.file_utils import load_wav
+from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
 
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
@@ -23,7 +24,66 @@ app.add_middleware(
 
 pool = None
 args = None
-
+def run_inference(model_dir, api, params):
+    try:
+        try:
+            model = CosyVoice(model_dir)
+        except Exception:
+            model = CosyVoice2(model_dir)
+        if api == "inference_sft":
+            tts_text, spk_id = params
+            result = []
+            for i in model.inference_sft(tts_text, spk_id):
+                tts_audio = (i['tts_speech'].numpy() * (2 ** 15)).astype('int16').tobytes()
+                result.append(tts_audio)
+            return result
+        elif api == "inference_zero_shot":
+            tts_text, prompt_text, prompt_wav_bytes = params
+            prompt_speech_16k = load_wav(io.BytesIO(prompt_wav_bytes), 16000)
+            result = []
+            for i in model.inference_zero_shot(tts_text, prompt_text, prompt_speech_16k):
+                tts_audio = (i['tts_speech'].numpy() * (2 ** 15)).astype('int16').tobytes()
+                result.append(tts_audio)
+            return result
+        elif api == "inference_cross_lingual":
+            tts_text, prompt_wav_bytes = params
+            prompt_speech_16k = load_wav(io.BytesIO(prompt_wav_bytes), 16000)
+            result = []
+            for i in model.inference_cross_lingual(tts_text, prompt_speech_16k):
+                tts_audio = (i['tts_speech'].numpy() * (2 ** 15)).astype('int16').tobytes()
+                result.append(tts_audio)
+            return result
+        elif api == "inference_instruct":
+            tts_text, spk_id, instruct_text = params
+            result = []
+            for i in model.inference_instruct(tts_text, spk_id, instruct_text):
+                tts_audio = (i['tts_speech'].numpy() * (2 ** 15)).astype('int16').tobytes()
+                result.append(tts_audio)
+            return result
+        elif api == "inference_instruct2":
+            tts_text, instruct_text, prompt_wav_bytes = params
+            prompt_speech_16k = load_wav(io.BytesIO(prompt_wav_bytes), 16000)
+            result = []
+            for i in model.inference_instruct2(tts_text, instruct_text, prompt_speech_16k):
+                tts_audio = (i['tts_speech'].numpy() * (2 ** 15)).astype('int16').tobytes()
+                result.append(tts_audio)
+            return result
+        elif api == "ws_tts":
+            texts, prompt_wav_bytes = params
+            prompt_speech_16k = load_wav(io.BytesIO(prompt_wav_bytes), 16000)
+            result = []
+            for i, j in enumerate(model.inference_zero_shot(
+                    iter(texts),
+                    "希望你以后能够做的比我还好呦。",
+                    prompt_speech_16k,
+                    stream=True)):
+                tts_audio = (j['tts_speech'].numpy() * (2 ** 15)).astype('int16').tobytes()
+                result.append(tts_audio)
+            return result
+        else:
+            return ["Unknown API"]
+    except Exception as e:
+        return [f"error:{str(e)}"]
 def stream_audio(result):
     for audio in result:
         if isinstance(audio, bytes):
